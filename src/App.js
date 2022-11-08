5fd4d6a507568b1a2716787284d01d466dca910e
Nisharga Kabir <nishargakabir@gmail.com>
2022-11-08 22:27:29 +0600

    react router implement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Mediagallery from "./Components/Mediagallery/Mediagallery";
+import Signup from "./Components/Signup/Signup";
+import Login from "./Components/Login/Login";
+import Errorpage from "./Components/Errorpage/Errorpage";
 import Medialibrary from "./Components/Medialibrary/Medialibrary";
 
 function App() {
   return (
     <div>
-      <Mediagallery />
+      <Routes>
+        <Route path="/" element={<Signup></Signup>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/medialibrary" element={<Medialibrary />} />
+        <Route path="*" element={<Errorpage />} />
+      </Routes>
     </div>
   );
 }
