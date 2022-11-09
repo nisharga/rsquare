6b77c72a87a8f34ec6cfa93be073ac0aa22540dc
Nisharga Kabir <nishargakabir@gmail.com>
2022-11-09 12:03:24 +0600

    deploy in vercel

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import Signup from "./Components/Signup/Signup";
 import Login from "./Components/Login/Login";
 import Errorpage from "./Components/Errorpage/Errorpage";
 import Medialibrary from "./Components/Medialibrary/Medialibrary";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  // const { isLoading, error, data } = useQuery(["repoData"], () =>
+  //   fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+  //     res.json()
+  //   )
+  // );
+
+  // if (isLoading) return "Loading...";
+
+  // if (error) return "An error has occurred: " + error.messag;
+
   return (
     <div>
       <Routes>
